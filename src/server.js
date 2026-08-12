27b45b02371926a305defad4dab06628b45782db
amrendrak788 <32064131+amrendrak788@users.noreply.github.com>
2026-08-12 20:00:36 +0530

    removing console logs

--- src/server.js
+++ src/server.js
@@ -11,15 +11,6 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, "0.0.0.0", async () => {
     console.log(`SafarDrop backend running on port ${port}`);
-
-    console.log("PostgreSQL environment:", {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        passwordSet: !!process.env.DB_PASSWORD
-    });
-
     try {
         await testConnection();
     } catch (err) {
